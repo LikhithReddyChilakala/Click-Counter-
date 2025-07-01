e82ed294600d367313cc049710bd3337eb01ec00
LikhithReddyChilakala <likhithreddy2423@gmail.com>
2025-07-01 22:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const counterElement = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
+
+let count = 0;
+
+incrementBtn.addEventListener('click', () => {
+    count++;
+    counterElement.textContent = count;
+});
